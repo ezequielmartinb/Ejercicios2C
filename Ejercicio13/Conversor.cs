cf842efeebc7530aa10943c1bbe86553aba73141
ezequielmartinb <ezequielmartinb10@gmail.com>
2023-04-06 20:43:08 -0300

    Ejercicio Resuelto

---
+++ Ejercicio13/Conversor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio13
+{
+    public class Conversor
+    {
+        public static string DecimalBinario(int numeroEntero) // Convertir de entero a binario
+        {
+            int auxNumeroEntero = numeroEntero;
+            int resto;
+            string numeroBinario = "";
+
+            while (auxNumeroEntero > 0)
+            {
+                resto = auxNumeroEntero % 2;
+                if (auxNumeroEntero % 2 == 0)
+                {
+                    numeroBinario = "0" + numeroBinario;
+                }
+                else
+                {
+                    numeroBinario = "1" + numeroBinario;
+                }
+                auxNumeroEntero = auxNumeroEntero / 2;
+
+            }
+            return numeroBinario;
+
+        }
+        public static int BinarioDecimal(string numeroEnBinario) // Convertir de binario a entero
+        {
+            double numeroDecimal = 0;
+            for (int i = 0; i < numeroEnBinario.Length; i++)
+            {
+                if (numeroEnBinario[i] == '1')
+                {
+                    numeroDecimal += Math.Pow(2, numeroEnBinario.Length - (i + 1));
+                }
+            }
+            return (int)numeroDecimal;
+        }
+    }
+}
---
+++ Ejercicio13/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ejercicio13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 13.	Desarrollar una clase llamada Conversor, que posea dos métodos de clase (estáticos):
+                    string DecimalBinario(int). Convierte un número de entero a binario.
+                    int BinarioDecimal(string). Convierte un número binario a entero.
+
+                    NOTA: no utilizar los atajos del lenguaje, hacerlo mediante estructuras repetitivas y de control.
+
+             */
+
+            int numeroDecimal = Conversor.BinarioDecimal("1110");
+            string numeroBinario = Conversor.DecimalBinario(14);
+            Console.WriteLine(numeroDecimal);
+            Console.WriteLine(numeroBinario);
+        }
+    }
+}
