77ed02fc64f22757db6c5e062a79b43bdaf22c0c
ezequielmartinb <ezequielmartinb10@gmail.com>
2023-04-08 14:04:50 -0300

    Ejercicio Resuelto

---
+++ Ejercicio16/Alumno.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio16
+{
+    public class Alumno
+    {
+        string apellido;
+        string nombre;
+        int legajo;
+        byte nota1;
+        byte nota2;
+        float notafinal;
+
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Legajo { get => legajo; set => legajo = value; }
+        public byte Nota1 { get => nota1; set => nota1 = value; }
+        public byte Nota2 { get => nota2; set => nota2 = value; }
+        public float Notafinal { get => notafinal; set => notafinal = value; }
+
+        public Alumno(string apellido, string nombre, int legajo)
+        {
+            Apellido = apellido;
+            Nombre = nombre;
+            Legajo = legajo;
+        }
+
+        public void Estudiar(byte notaUno, byte notaDos)
+        {
+            Nota1 = notaUno;
+            Nota2 = notaDos;
+        }
+        public void CalcularFinal()
+        {
+            if (Nota1 >= 4 && Nota2 >= 4)
+            {
+                Notafinal = new Random().Next(4, 10);
+            }
+            else
+            {
+                Notafinal = -1;
+            }
+        }
+        public string Mostrar()
+        {
+            string mensaje;
+
+            if (this.Notafinal == -1)
+            {
+                mensaje = $"El alumno {Apellido},{Nombre} legajo nro {Legajo} tiene como Nota 1: {Nota1} y como Nota 2: {Nota2}. ALUMNO DESAPROBADO";
+            }
+            else
+            {
+                mensaje = $"El alumno {Apellido},{Nombre} legajo nro {Legajo} tiene como Nota 1: {Nota1} y como Nota 2: {Nota2}. Su nota final es: {Notafinal}";
+            }
+
+            return mensaje;
+        }
+    }
+}
---
+++ Ejercicio16/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Ejercicio16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*          a.	Se pide crear 3 instancias de la clase Alumno (3 objetos) en la función Main. 
+             *              Colocarle nombre, apellido y legajo a cada uno de ellos.
+             *              
+                        b.	Sólo se podrá ingresar las notas (nota1 y nota2) a través del método Estudiar.
+
+                        c.	El método CalcularFinal deberá colocar la nota del final sólo si las notas 1 y 2 son mayores 
+                            o iguales a 4,caso contrario la inicializará con -1. 
+                            Para darle un valor a la nota final utilice el método de instancia Next de la clase Random.
+
+                        d.	El método Mostrar, expondrá en la consola todos los datos de los alumnos. 
+                            La nota final se mostrará sólo si es distinto de -1, 
+                            caso contrario se mostrará la leyenda "Alumno desaprobado".
+
+             */
+            Alumno alumno1 = new Alumno("JUAN", "PEREZ", 11101);
+            Alumno alumno2 = new Alumno("ROBERTO", "GOMEZ", 12003);
+            Alumno alumno3 = new Alumno("EZEQUIEL", "BRAÑANOVA", 41101);
+
+            alumno1.Estudiar(4, 2);
+            alumno2.Estudiar(3, 4);
+            alumno3.Estudiar(9, 10);
+
+            alumno1.CalcularFinal();
+            alumno2.CalcularFinal();
+            alumno3.CalcularFinal();
+
+            Console.WriteLine(alumno1.Mostrar());
+            Console.WriteLine(alumno2.Mostrar());
+            Console.WriteLine(alumno3.Mostrar());
+
+
+        }
+    }
+}
