1192940d76ea4ae28f5ad3a07ec595ae9c70fbc2
ezequielmartinb <ezequielmartinb10@gmail.com>
2023-04-17 19:47:27 -0300

    Ejercicios Resueltos

---
+++ Ejercicio19/Program.cs
@@ -0,0 +1,24 @@
+﻿using Entidades;
+
+namespace Ejercicio19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sumador sumador = new Sumador();
+            Sumador otroSumador = new Sumador();
+
+            Console.WriteLine(sumador.Sumar(4, 20));
+            Console.WriteLine(sumador.Sumar("Juan", "Carlos"));
+            Console.WriteLine(((int)sumador));
+
+            Console.WriteLine(otroSumador.Sumar(72, 8));
+            Console.WriteLine(otroSumador.Sumar("HOLA", "MUNDO"));
+
+            Console.WriteLine(sumador + otroSumador);
+            Console.WriteLine(sumador | otroSumador);
+
+        }
+    }
+}
