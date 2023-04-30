6f7e14f5610a2337f5419519801bf5a682ae1417
ezequielmartinb <ezequielmartinb10@gmail.com>
2023-04-30 11:15:09 -0300

    Ejercicio Resuelto

---
+++ Ejercicio35/Program.cs
@@ -0,0 +1,26 @@
+﻿using Entidades;
+
+namespace Ejercicio35
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona unaPersona = new Persona(37332399, "Ezequiel Brañanova");
+            DirectorTecnico unDirectorTecnico = new DirectorTecnico("Juan Gelvez", new DateTime(1977, 01, 19));
+            DirectorTecnico otroDirectorTecnico = new DirectorTecnico("Juan Gelvez", new DateTime(1977, 01, 19));
+
+            Console.WriteLine(unaPersona.MostrarDatos());
+            Console.WriteLine(unDirectorTecnico.MostrarDatos());
+
+            if(unDirectorTecnico == otroDirectorTecnico)
+            {
+                Console.WriteLine("Hay dos DT iguales");
+            }
+            else
+            {
+                Console.WriteLine("No son iguales");
+            }
+        }
+    }
+}
